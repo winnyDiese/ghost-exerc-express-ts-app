d55d33aa598c0b5775433f481e70f8675ff8b28a
winnyDiese <winnydiese@gmail.com>
2025-01-07 16:10:47 +0100

    lkjb

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import express, {Request, Response} from "express"
 const app = express()
 const port = 3000
 
-app.get('/', (req:Request, res: Response) => {
-    res.send('<h1>Bienvenue sur ma page Express avec TypeScript !</h1>');
-} )
+app.set('view engine','ejs')
+app.set('views','./views')
+
+
+// app.get('/', (req:Request, res: Response) => {
+//     res.send('<h1>Bienvenue sur ma page Express avec TypeScript !</h1>');
+// } )
+
+
 
 app.listen(port, ()=>{
     console.log(`Serveur lancé sur http://localhost:${port}`)
